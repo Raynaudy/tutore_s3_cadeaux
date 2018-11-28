e18cec102f004f8f6154501a34218e006c297056
Yvain Raynaud <yvain.raynaud@gmail.com>
2018-11-28 23:24:41 +0100

    add index redirect

--- web/navbar.inc.php
+++ web/navbar.inc.php
@@ -2,7 +2,7 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"><i class="fas fa-bars fa-lg"></i></span>
   </button>
-  <a class="navbar-brand " href="#"><span class="mb-0 h2 text-danger "><b>Gift</b>list</span></a>
+  <a class="navbar-brand " href="index.php"><span class="mb-0 h2 text-danger "><b>Gift</b>list</span></a>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto">
       <li class="form-inline my-2 my-lg-0">
