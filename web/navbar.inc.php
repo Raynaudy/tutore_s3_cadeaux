5377794b2dead6b1cb71b1d86b5005c34baccda4
Yvain Raynaud <yvain.raynaud@gmail.com>
2018-11-28 23:16:13 +0100

    add fa and color

--- web/navbar.inc.php
+++ web/navbar.inc.php
@@ -1,29 +1,16 @@
 <nav class="navbar navbar-expand-lg transp ">
-  <a class="navbar-brand " href="#"><span class="mb-0 h3 text-dark "><b>Gift</b>list</span></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <span class="navbar-toggler-icon"><i class="fas fa-bars fa-lg"></i></span>
   </button>
+  <a class="navbar-brand " href="#"><span class="mb-0 h2 text-danger "><b>Gift</b>list</span></a>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active ">
-        <a class="nav-link text-dark" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="#">Pricing</a>
-      </li>
-    </ul>
     <ul class="navbar-nav ml-auto">
-        <li class="form-inline my-2 my-lg-0">
-          <a href="signup.php" class="btn btn-outline-danger my-2 my-sm-0 mr-2">S'inscrire</a>
-        </li> 
-
-       <li class="form-inline my-2 my-lg-0">
-          <a href="login.php" class="btn btn-outline-danger my-2 my-sm-0" >Se connecter</a>
-        </li> 
+      <li class="form-inline my-2 my-lg-0">
+        <a href="signup.php" class="btn btn-outline-danger my-2 my-sm-0 mr-2">S'inscrire</a>
+      </li> 
+    <li class="form-inline my-2 my-lg-0">
+        <a href="login.php" class="btn btn-danger  my-2 my-sm-0" >Se connecter</a>
+      </li> 
     </ul>
-
   </div>
 </nav>
