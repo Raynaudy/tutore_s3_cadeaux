a4a5ba297d95a43f47fe539ff7c555099f2dde6d
Raynaudy <Raynaudy@users.noreply.github.com>
2018-11-29 18:55:07 +0100

    cahnge header

--- web/www/includes/header.inc.php
+++ web/www/includes/header.inc.php
@@ -1,3 +1,22 @@
-<header class="masthead text-white text-center">
-   
+<header class="masthead ">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="content-box">
+                </div>
+            </div>
+        <div class="col-sm-6">
+            <div class="content-box absolute">
+                <div class="text-right red">
+                    <div class="h1">Bienvenue, </div>
+                    <br>
+                    <div class="h2">N'achetez plus jamais de cadeau en double grace à <b>Gift</b>list</div>
+
+                
+                   
+            
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
