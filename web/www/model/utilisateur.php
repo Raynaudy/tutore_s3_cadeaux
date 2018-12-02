839a14a5451bb8911d068629b84cbc5525eee772
Eugene Leclerc <eugeneleclerc@orange.fr>
2018-12-02 18:17:41 +0100

    Continuation de utilisateur.php

--- web/www/model/utilisateur.php
+++ web/www/model/utilisateur.php
@@ -1,14 +1,40 @@
 <?php
 
-class Utilisateur {
-  private id;
-  private login;
-  private mdp;
-  private nom;
-  private prenom;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once('../controller/connect.php');
 
-  public function _construct(){
-    
+class Utilisateur {
+  private $connection;
+  private $id;
+  private $login;
+  private $mdp;
+  private $nom;
+  private $prenom;
+
+  function __construct(){
+    $nbArgs = func_num_args();
+    $args = func_get_args();
+    switch($nbArgs){
+      case 5 : 
+        $this->connection = $args[0];
+        $this->login = $args[1];
+        $this->mdp = $args[2];
+        $this->nom = $args[3];
+        $this->prenom = $args[4];
+
+      case 3 :
+        $this->connection = $args[0];
+        $this->login = $args[1];
+        $this->mdp = $args[2];
+    }
+  }
+
+  public function checkMdp(){
+    $getMdp = "SELECT mdp FROM UtilisateurActif WHERE login = '$this->login'";
+    $result = mysqli_query($this->connection,$getMdp);
+    $result = mysqli_fetch_assoc($result);
+    return (password_verify($this->mdp, $result['mdp']));
   }
 }
 
