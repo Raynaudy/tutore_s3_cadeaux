45ba3ad6c15ddf3ee1a3d2ed880243758e0a280d
Raynaudy <Raynaudy@users.noreply.github.com>
2018-12-12 17:52:54 +0100

    elenver login inactif

--- web/www/view/modifInfos.php
+++ web/www/view/modifInfos.php
@@ -52,7 +52,6 @@
                   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                     <a class="dropdown-item" href="modifInfos.php">Voir mon profil</a>
                     <a class="dropdown-item" href="#">Créer un compte invité</a>
-                    <a class="dropdown-item" href="loginInactif.php">Se loguer en tant que xxx </a>
                     <a class="dropdown-item" href="../controller/logout.php">Se déconnecter</a>
                   </div>
                 </div>
