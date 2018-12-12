ecb4a0b750f97446801653247b42b57290fa68a8
Lise Jolicoeur <lisejoli@free.fr>
2018-12-12 21:19:04 +0100

    ajouter cadeau inactif

---
+++ web/www/controller/ajouterCadeauListeInactif.php
@@ -0,0 +1,12 @@
+<?php
+
+    require_once("connect.php");
+    
+    $id_liste = $_POST['id_liste'];
+   
+    foreach($_POST['cadeau'] as $selected)
+    {
+        mysqli_query($co,"INSERT INTO fait_partie(id_cadeau,id_liste) VALUES ('$selected','$id_liste')");
+    }
+    header("Location:../view/listes.php");
+?>
