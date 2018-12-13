df4b1f17ca07e43b0350ec2eda4eb0deaee00403
Eugene Leclerc <eugeneleclerc@orange.fr>
2018-12-13 15:21:58 +0100

    Correction id_utilisateur_est_offert

--- web/www/controller/acheterCadeau.php
+++ web/www/controller/acheterCadeau.php
@@ -5,7 +5,7 @@
     $id_cadeau = $_GET['id_cadeau'];
     $id_util = $_SESSION['id_utilisateur'];
     
-    mysqli_query($co,"UPDATE Cadeau SET id_utilisateur_est_offert = '$id_util' WHERE id_cadeau = '$id_cadeau'");
+    mysqli_query($co,"UPDATE Cadeau SET id_utilisateur = '$id_util' WHERE id_cadeau = '$id_cadeau'");
     
     header("Location:../view/listes.php");
 ?>
